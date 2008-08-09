14d7e28862b0a7a426b696e0d2c74290b832326b
Z Dravko <zdravko@openlinksw.com>
2008-08-09 13:19:11 +0000

    Latest changes.

--- VirtTripleLoader/src/virtTripleLoaderInit/VirtTripleLoaderInit.java
+++ VirtTripleLoader/src/virtTripleLoaderInit/VirtTripleLoaderInit.java
@@ -60,7 +60,7 @@ import virtuoso.jdbc3.*;
 
 public class VirtTripleLoaderInit {
 
-	private static final Boolean debugMode = true;
+	private static final Boolean debugMode = false;
 	private static final String myName = "Virtuoso Triple Loader Java";
 	private static final Integer myMajorVer = 0;
 	private static final Integer myMinorVer = 1;
@@ -90,38 +90,24 @@ public class VirtTripleLoaderInit {
 
 	private class WarcBlockWriter {
 		private Integer fileNo = 0;
-		private String fileFormat = "triples_%06d.nt";
+		private String fileFormat = "data%01d/triples_%07d.nt";
 		private String curFile;
 		private FileOutputStream outStream;
 		private byte[] buf = new byte[2048];
 		private int bytesRead;
 		private int bytesInFile;
 
-		public String write(MeasurableInputStream _block) throws IOException {
+		public String write(MeasurableInputStream _block, int cnt, int step) throws IOException {
 			bytesInFile = 0;
-			curFile = String.format(fileFormat, fileNo++);
+			curFile = String.format(fileFormat, step, cnt);
 			outStream = new FileOutputStream(curFile);
-/*
-			while ((bytesRead = _block.read (buf)) != -1) {
-				bytesInFile += bytesRead;
-				outStream.write(buf);
-			}
-*/
+
 			int in_url = 0;
 			int c = 0;
 			while ((c = _block.read()) != -1) {
 
 				bytesInFile ++;
-/*			    	if (c == 60) {
-				    in_url = 1;
-				}
-			    	if (c == 62 && in_url==1) {
-				    in_url = 0;
-				}
-				if (in_url==1 && c==32) {
-				    c=95;
-				}*/
-				outStream.write(c);
+				outStream.write(c); 
 
 			}
 
@@ -159,14 +145,8 @@ public class VirtTripleLoaderInit {
 
 //			 TODO: Use file to store secret. Check file perms and refuse to work if not restrictive enough
 
-			System.out.println (virtConnectUrl);
-			System.out.println (virtUser);
-			System.out.println (virtPwd);
-
 			virtConnection =
 				DriverManager.getConnection(virtConnectUrl,virtUser,virtPwd);
-
-			System.out.println (virtConnection);
 
 			ldAddStmt = virtConnection.prepareStatement("ld_add (?,?)");
 
@@ -207,146 +187,148 @@ public class VirtTripleLoaderInit {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		VirtTripleLoaderInit vtl = new VirtTripleLoaderInit ();
+	    VirtTripleLoaderInit vtl = new VirtTripleLoaderInit ();
 
-		if (args.length < 1 || args.length > 2) {
-			printUsage();
-			l.error("WX0002: Invalid number of arguments. Exiting.");
-			System.exit(-1);
-		}
+	    if (args.length < 1 || args.length > 2) {
+		printUsage();
+		l.error("WX0002: Invalid number of arguments. Exiting.");
+		System.exit(-1);
+	    }
 
-		if (args.length == 2) {
+	    if (args.length == 2) {
 
-		}
+	    }
 
-		String inFile = args[0];
+	    String inFile = args[0];
+	    int start = Integer.parseInt (args[1]);
+	    int step = 1000000;
+	    int min = start * step;
+	    int max = min + step;
 
-		l.output("START " + inFile);
+	    System.out.println (start);
+	    l.output("START " + inFile);
 
-		final FastBufferedInputStream in =
-			new FastBufferedInputStream(new FileInputStream (new File(inFile)));
+	    final FastBufferedInputStream in =
+		new FastBufferedInputStream(new FileInputStream (new File(inFile)));
 
-		GZWarcRecord record = new GZWarcRecord();
-		Filter<WarcRecord> filter = Filters.adaptFilterBURL2WarcRecord (new TrueFilter());
-		WarcFilteredIterator it = new WarcFilteredIterator(in, record, filter);
+	    GZWarcRecord record = new GZWarcRecord();
+	    Filter<WarcRecord> filter = Filters.adaptFilterBURL2WarcRecord (new TrueFilter());
+	    WarcFilteredIterator it = new WarcFilteredIterator(in, record, filter);
 
-		WarcHttpResponse response = new WarcHttpResponse();
+	    WarcHttpResponse response = new WarcHttpResponse();
 
-		Graph mdGraph = new org.openrdf.model.impl.GraphImpl();
-		String mdGraphURI = "http://challenge.semanticweb.org/2008/metadata";
-		ValueFactory vf = mdGraph.getValueFactory();
-		String dcNS = "http://purl.org/dc/elements/1.1/";
-		DatatypeFactory dtf = null;
+	    Graph mdGraph = new org.openrdf.model.impl.GraphImpl();
+	    String mdGraphURI = "http://challenge.semanticweb.org/2008/metadata";
+	    ValueFactory vf = mdGraph.getValueFactory();
+	    String dcNS = "http://purl.org/dc/elements/1.1/";
+	    DatatypeFactory dtf = null;
 
-		try {
-			dtf = DatatypeFactory.newInstance();
-		} catch (DatatypeConfigurationException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+	    try {
+		dtf = DatatypeFactory.newInstance();
+	    } catch (DatatypeConfigurationException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	    }
 
-		GregorianCalendar c = new GregorianCalendar ();
+	    GregorianCalendar c = new GregorianCalendar ();
 
-		try {
-			int cnt = 0;
+	    try {
+		int cnt = 0;
 
-//			while (cnt < 1000 && it.hasNext()) {
-			while (it.hasNext()) {
+		//			while (cnt < 10 && it.hasNext()) {
+		while (it.hasNext()) {
 
-				WarcRecord nextRecord = it.next();
+		    WarcRecord nextRecord = it.next();
 
-				l.output ("cnt " + cnt);
-				l.output ("nextRecord " + nextRecord);
+		    //Get the HttpResponse
+		    try {
+			response.fromWarcRecord (nextRecord);
 
-				//Get the HttpResponse
-				try {
-					response.fromWarcRecord (nextRecord);
-
-					if (debugMode) {
-						System.out.println("RECORD     : " + String.format("%05d", cnt));
-						System.out.println(" subjectUri: " + nextRecord.header.subjectUri);
-						System.out.println("contentType: " + nextRecord.header.contentType);
-						System.out.println(" dataLength: " + nextRecord.header.dataLength);
-						System.out.println("actual data: " + nextRecord.block.length());
-						System.out.println("    missing: " +
-								(nextRecord.header.dataLength - nextRecord.block.length()) + "b");
-					}
-					l.output(nextRecord.header.subjectUri.toString());
-
-					URI s, p, o;
-					Literal lit;
-
-					s = vf.createURI(nextRecord.header.subjectUri.toString());
-					p = vf.createURI(dcNS, "source");
-					lit = vf.createLiteral(inFile);
-
-					mdGraph.add(s,p,lit);
-
-					c.setTime(nextRecord.header.creationDate);
-					XMLGregorianCalendar xc = dtf.newXMLGregorianCalendar(c);
-
-					p = vf.createURI(dcNS, "date");
-					lit = vf.createLiteral(xc);
-
-					mdGraph.add(s,p,lit);
-
-//					p = vf.createURI("http://")
-
-					curFile = w.write(response.contentAsStream());
-					l.output ("curFile " + curFile);
-
-					try {
-						ldAddStmt.setString(1, curFile);
-						ldAddStmt.setString(2, nextRecord.header.subjectUri.toString());
-						ResultSet res = ldAddStmt.executeQuery();
-					}
-					catch (SQLException e) {
-						l.error(e);
-						l.error("WX0003: Insert to loader table failed. Exiting.");
-						System.exit(-1);
-					}
-				}
-				catch (IOException e) {
-					e.printStackTrace();
-					continue;
-				}
-				cnt++;
+			if (debugMode) {
+			    System.out.println("RECORD     : " + String.format("%05d", cnt));
+			    System.out.println(" subjectUri: " + nextRecord.header.subjectUri);
+			    System.out.println("contentType: " + nextRecord.header.contentType);
+			    System.out.println(" dataLength: " + nextRecord.header.dataLength);
+			    System.out.println("actual data: " + nextRecord.block.length());
+			    System.out.println("    missing: " +
+				    (nextRecord.header.dataLength - nextRecord.block.length()) + "b");
 			}
-		}
+			l.output(nextRecord.header.subjectUri.toString());
 
+			URI s, p, o;
+			Literal lit;
+			if (cnt == max ){ return ; }
+			if (cnt >= min && cnt < max) {
 
-		catch (RuntimeException re) {
-			l.error ("WX0004 Unexpected Runtime Error Thrown.");
-			l.error (re.toString());
-			re.printStackTrace();
-			System.exit (-1);
-		}
-				l.output ("Finish loop ");
+			    s = vf.createURI(nextRecord.header.subjectUri.toString());
+			    p = vf.createURI(dcNS, "source");
+			    lit = vf.createLiteral(inFile);
 
-		Iterator<Statement> iter = mdGraph.iterator();
-		Integer _stmtcnt = 0;
-		String mdFile = String.format("%s.md.nt", inFile);
+			    mdGraph.add(s,p,lit);
 
-		FileOutputStream outStream = new FileOutputStream(mdFile);
-		NTriplesWriter ntw = new NTriplesWriter (outStream);
+			    c.setTime(nextRecord.header.creationDate);
+			    XMLGregorianCalendar xc = dtf.newXMLGregorianCalendar(c);
 
-		ntw.handleNamespace ("dc", dcNS);
+			    p = vf.createURI(dcNS, "date");
+			    lit = vf.createLiteral(xc);
 
-		try {
-			ntw.startRDF();
-			while (iter.hasNext()) {
-				Statement _stmt = iter.next();
-				ntw.handleStatement(_stmt);
-				_stmtcnt++;
+			    mdGraph.add(s,p,lit);
+
+			    curFile = w.write(response.contentAsStream(), cnt, start);
+
+			    try {
+				ldAddStmt.setString(1, curFile);
+				ldAddStmt.setString(2, nextRecord.header.subjectUri.toString());
+				ResultSet res = ldAddStmt.executeQuery();
+			    }
+			    catch (SQLException e) {
+				l.error(e);
+				l.error("WX0003: Insert to loader table failed. Exiting.");
+				System.exit(-1);
+			    }
 			}
-			ntw.endRDF();
-			outStream.close();
-		}
-		catch (Exception e) {
+		    }
+		    catch (IOException e) {
 			e.printStackTrace();
-			System.exit (-1);
+			continue;
+		    }
+		    cnt++;
+		}
 		}
 
-		l.output(_stmtcnt + " statements created in metadata graph " + mdGraphURI);
+
+	    catch (RuntimeException re) {
+		l.error ("WX0004 Unexpected Runtime Error Thrown.");
+		l.error (re.toString());
+		re.printStackTrace();
+		System.exit (-1);
+	    }
+	    l.output ("Finish loop ");
+
+	    Iterator<Statement> iter = mdGraph.iterator();
+	    Integer _stmtcnt = 0;
+	    String mdFile = String.format("%s.md.nt", inFile);
+
+	    FileOutputStream outStream = new FileOutputStream(mdFile);
+	    NTriplesWriter ntw = new NTriplesWriter (outStream);
+
+	    ntw.handleNamespace ("dc", dcNS);
+
+	    try {
+		ntw.startRDF();
+		while (iter.hasNext()) {
+		    Statement _stmt = iter.next();
+		    ntw.handleStatement(_stmt);
+		    _stmtcnt++;
+		}
+		ntw.endRDF();
+		outStream.close();
+	    }
+	    catch (Exception e) {
+		e.printStackTrace();
+		System.exit (-1);
+	    }
+
+	    l.output(_stmtcnt + " statements created in metadata graph " + mdGraphURI);
+	    }
 	}
-}
