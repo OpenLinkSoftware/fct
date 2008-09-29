0ed27e6f0c58503e3fa8354f87ebffdebfbc0ace
Orri Erling <oerling@openlinksw.com>
2008-09-29 15:10:25 +0000

    ns

---
+++ ns.sql
@@ -0,0 +1,20 @@
+
+
+
+
+DB.DBA.XML_SET_NS_DECL ('foaf', 'http://xmlns.com/foaf/0.1/', 2)
+;
+DB.DBA.XML_SET_NS_DECL ('dc', 'http://purl.org/dc/elements/1.1/', 2)
+;
+DB.DBA.XML_SET_NS_DECL ('xsd', 'http://www.w3.org/2001/XMLSchema-datatypes/', 2)
+;
+DB.DBA.XML_SET_NS_DECL ('rev', 'http://purl.org/stuff/rev#', 2)
+;
+DB.DBA.XML_SET_NS_DECL ('sioc', 'http://rdfs.org/sioc/ns#', 2);
+
+
+DB.DBA.XML_SET_NS_DECL ('geo', 'http://www.geonames.org/ontology#', 2);
+
+DB.DBA.XML_SET_NS_DECL ('pos', 'http://www.w3.org/2003/01/geo/wgs84_pos#', 2);
+
+DB.DBA.XML_SET_NS_DECL ('usc',  'http://www.rdfabout.com/rdf/schema/uscensus/details/100pct/', 2);
