da6a2455cbd256f8f292838544bb3e8d987532c7
Mitko Iliev <imitko@openlinksw.com>
2009-01-14 13:22:58 +0000

    json & sparql-results format support

--- facet_svc.sql
+++ facet_svc.sql
@@ -4,10 +4,62 @@ cl_exec ('registry_set (''fct_max_timeout'',''10000'')');
 DB.DBA.VHOST_REMOVE (lpath=>'/fct/service');
 DB.DBA.VHOST_DEFINE (lpath=>'/fct/service', ppath=>'/SOAP/Http/FCT_SVC', soap_user=>'dba');
 
+create procedure fct_svc_exec (in tree any, in timeout int, in accept varchar)
+{
+  declare start_time int;
+  declare sqls, msg, qr, qr2, act varchar;
+  declare ret, md, res, xmlout any;
+
+  xmlout := 1;
+  if (accept like '%/sparql-results+%' or accept = 'application/json')
+    xmlout := 0;
+  set result_timeout = timeout;
+  sqls := '00000';
+  ret := '';
+  qr := fct_query (xpath_eval ('//query', tree, 1));
+  if (xmlout)
+    qr2 := fct_xml_wrap (tree, qr);
+  else
+    qr2 := 'sparql define output:valmode "LONG" ' || qr;
+  start_time := msec_time ();
+  --http (qr2);
+  exec (qr2, sqls, msg, vector (), 0, md, res);
+  act := db_activity ();
+  if (sqls <> '00000' and sqls <> 'S1TAT')
+    signal (sqls, msg);
+
+  set result_timeout = 0;
+
+  if (xmlout)
+    {
+      if (not isarray (res) or 0 = length (res) or not isarray (res[0]) or 0 = length (res[0]))
+	res := vector (vector (xtree_doc ('<result/>')));
+
+      ret := xmlelement ("facets", xmlelement ("sparql", qr), xmlelement ("time", msec_time () - start_time),
+			   xmlelement ("complete", case when sqls = 'S1TAT' then 'no' else 'yes' end),
+			   xmlelement ("timeout", timeout),
+			   xmlelement ("db-activity", act), res[0][0]);
+      ret := xslt ('file:///fct/fct_resp.xsl', ret);
+    }
+  else
+    {
+      declare ses, tmp any;
+      ses := string_output ();
+      DB.DBA.SPARQL_RESULTS_WRITE (ses, md, res, accept, 1);
+      ret := ses;
+    }
+  return ret;
+}
+;
+
 create procedure fct_svc () __soap_http 'text/xml'
 {
-  declare cnt, tp, ret, timeout, xt, xslt, maxt, tmp any;
-  tp := http_request_header (http_request_header (), 'Content-Type');
+  declare cnt, tp, ret, timeout, xt, xslt, maxt, tmp, lines, accept any;
+
+  lines := http_request_header ();
+  tp := http_request_header (lines, 'Content-Type');
+  accept := http_request_header_full (lines, 'Accept', '*/*');
+  accept := DB.DBA.HTTP_RDF_GET_ACCEPT_BY_Q (accept);
   if (tp <> 'text/xml')
     {
       http_status_set (500);
@@ -36,8 +88,7 @@ create procedure fct_svc () __soap_http 'text/xml'
   maxt := atoi (registry_get ('fct_max_timeout'));
   if (timeout > maxt)
     timeout := maxt;
-  ret := fct_exec (xslt, timeout);
-  ret := xslt ('file:///fct/fct_resp.xsl', ret);
+  ret := fct_svc_exec (xslt, timeout, accept);
   return ret;
 }
 ;
