6663520666c9a19c7339fc5e795482fc282c0cda
Mitko Iliev <imitko@openlinksw.com>
2009-05-18 13:43:26 +0000

    *** empty log message ***

--- sum.sql
+++ sum.sql
@@ -7,6 +7,7 @@ create procedure s_sum_init (inout env any)
 {
   env := make_array (30, 'any');
 }
+;
 
 create procedure s_sum_acc (inout env any, in s_rank double precision, in p iri_id, in o any, in sc int)
 {
@@ -22,12 +23,14 @@ create procedure s_sum_acc (inout env any, in s_rank double precision, in p iri_
   env[fill + 5] := sc;
   env[1] := fill + 3;
 }
+;
 
 
 create procedure s_sum_fin (inout env any)
 {
   return env;
 }
+;
 
 
 create aggregate DB.DBA.S_SUM (in s_rank double precision, in p iri_id, in o any, in sc int) returns any from 
@@ -37,6 +40,7 @@ create procedure sum_rank (inout arr any)
 {
   return  rnk_scale (arr[0]) + cast (arr[2] as real) / (arr[1] / 3);
 }
+;
 
 
 create procedure sum_o_p_score (inout o any, inout p any)
@@ -57,20 +61,7 @@ create procedure sum_o_p_score (inout o any, inout p any)
     return lng_m + pm;
   return lng_m;
 }
-
-
-create procedure s_summary (inout row any)
-{
-  declare sorted, inx, tot any;
- tot := '';
- sorted := subseq (row, 3, row[1] + 3);
-  for (inx := 0; inx < length (sorte); inx := inx + 3)
-    sorted[inx + 2] := sum_o_p_score (sorted[inx], sorted[inx + 1]);
-  gvector_sort (sorted, 3, 2, 0);
-  for (inx := 0; inx < length (sorted); inx := inx + 3)
-  tot	 := tot || rdf_box_data (sorted[inx]);
-  return search_excerpt (text_exp, tot);
-}
+;
 
 
 create procedure sum_result (inout final any, inout res any, inout text_exp any, inout s varchar, inout start_inx int, inout end_inx int, inout s_rank real)
@@ -95,12 +86,14 @@ create procedure sum_result (inout final any, inout res any, inout text_exp any,
 		    xmlelement ('col', cast (cast (tsum as real) / ((end_inx - start_inx) / 3) as varchar)));
   xte_nodebld_xmlagg_acc (final, elt);
 }
+;
 
 
 create procedure sum_final (inout x any)
 {
   return xml_tree_doc (xte_nodebld_final_root (x));
 }
+;
 
 
 create procedure s_sum_page (in rows any, in text_exp varchar)
@@ -141,7 +134,7 @@ create procedure s_sum_page (in rows any, in text_exp varchar)
   sum_result (final, res, text_exp, s, prev_fill, fill, s_rank);
   return sum_final (final);
 }
-
+;
 
 create procedure vt ()
 {
@@ -150,19 +143,23 @@ create procedure vt ()
   gvector_sort (x, 2, 1, 1);
   --dbg_obj_print (x);
 }
+;
 
 create procedure sum_tst (in text_exp varchar, in text_words varchar := null)
 {
   declare res any;
   if  (text_words is null)
     text_words := vector (text_exp);
- res := (select vector_agg (vector (s, sm)) from 
-  (select top 20 s, s_sum (iri_rank (s), p, o, score)  as sm from rdf_obj, rdf_ft, rdf_quad q1 where contains (ro_flags, text_exp) and rf_id = ro_id and q1.o = rf_o group by s 
+  res := (select vector_agg (vector (s, sm)) from 
+  (select top 20 s, s_sum (iri_rank (s), p, o, score)  as sm 
+   from rdf_obj, rdf_ft, rdf_quad q1 
+   where contains (ro_flags, text_exp) and rf_id = ro_id and q1.o = rf_o group by s 
    order by sum_rank (sm) option (quietcast) ) s option (quietcast));
-  dbg_obj_print (res);
- res := s_sum_page (res, text_words);
+  --dbg_obj_print (res);
+  res := s_sum_page (res, text_words);
   return res;
 }
+;
 
 --  sum_tst ('oori');
 
