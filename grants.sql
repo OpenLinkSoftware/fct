f714b581f23d8ad0748c98d6350ca3ce891888a8
Mitko Iliev <imitko@openlinksw.com>
2012-11-05 14:06:42 +0000

    exec is as user executing the page e.g. SPARQL

--- grants.sql
+++ grants.sql
@@ -49,3 +49,4 @@ grant execute on rdfdesc_http_url to "SPARQL_SELECT";
 grant execute on fct_sparql_ser to "SPARQL_SELECT";
 grant execute on b3s_lbl_order to SPARQL_SELECT;
 grant execute on DB.DBA.DAV_SEARCH_PATH to "SPARQL_SELECT";
+grant execute on DB.DBA.VT_INC_INDEX_DB_DBA_RDF_OBJ to "SPARQL_SELECT";
