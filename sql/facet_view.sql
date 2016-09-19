342bf68fe810db81a7a7ebe56837c767fcee7e48
Patrick van Kleef <pkleef@openlinksw.com>
2016-09-19 11:16:30 +0200

    Fixed issue with xslt path

--- sql/facet_view.sql
+++ sql/facet_view.sql
@@ -24,7 +24,7 @@
 -- Facets web page
 
 registry_set ('_fct_xslt_', 
-              case when registry_get('_fct_url_') = 0 then 'file://fct/' else registry_get('_fct_url_') end);
+              case when registry_get('_fct_url_') = 0 then 'file://fct/' else registry_get('_fct_url_') end || 'xslt/');
 
 
 create procedure
@@ -2627,14 +2627,37 @@ create procedure fct_page_head ()
 
 create procedure fct_desc_page_head ()
 {
-  http ('<div id="hd_l">
-    <h1 id="logo">
-        <a href="/fct/facet.vsp">
-	   <img src="/fct/images/openlink_site_logo.png" alt="OpenLink Software"/>
-        </a>
-    </h1>
-    <div id="homelink"></div>
-  </div> <!-- hd_l -->
+  http ('
+    <div class="navbar-header">
+	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#fct-navbar" aria-expanded="false">
+	    <span class="sr-only">Toggle navigation</span>
+	    <span class="icon-bar"></span>
+	    <span class="icon-bar"></span>
+	    <span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="/fct" title="Faceted Browser">Faceted Browser</a>
+	<div id="homelink"></div>
+    </div> <!-- navbar-header -->
+  ');
+
+  http ('
+    <div class="collapse navbar-collapse" id="fct-navbar">
+      <ul class="nav navbar-nav">
+	<li class="dropdown">
+	    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span> Browse using <span class="caret"></span></a>
+	    <ul class="dropdown-menu">
+	      <li><a href="/describe/?uri=<?vsp http_url (page_resource_uri); ?>">OpenLink Faceted Browser</a></li>
+	      <li><a href="http://linkeddata.uriburner.com/rdf-editor/#/editor?uri=<?vsp http_url(page_resource_uri); ?>&amp;view=statements">OpenLink Structured Data Editor</a></li>
+	      <li><a href="http://en.lodlive.it/?<?vsp http_url (page_resource_uri); ?>">LodLive Browser</a></li>
+	    </ul>
+	</li>
+	<li class="dropdown">
+	    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Formats <span class="caret"></span></a>
+	    <ul class="dropdown-menu">
+	    <ul>
+	</li>
+      </ul>
+    </div> <!--navbar-collapse -->
   ');
 }
 ;
