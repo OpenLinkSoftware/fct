829fa39ed80e538affdae1278264c002fe267ef5
Patrick van Kleef <pkleef@openlinksw.com>
2017-08-13 16:44:32 +0200

    Fixed adding endpoints to both *ini* and *sslini*

--- sql/cl_prx.sql
+++ sql/cl_prx.sql
@@ -20,10 +20,6 @@
 --  51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 --
 
---DB.DBA.VHOST_REMOVE (lpath=>'/proxy');
---DB.DBA.VHOST_DEFINE (lpath=>'/proxy', ppath=>'/SOAP/Http/EXT_HTTP_PROXY_1', soap_user=>'PROXY',
---      opts=>vector('url_rewrite', 'ext_http_proxy_rule_list1'));
-
 create procedure
 ext_http_proxy_1 (in "url" varchar := null,
                 in exec varchar := null,
@@ -228,36 +224,3 @@ end_loop:;
 grant execute on ext_http_proxy_1 to PROXY
 ;
 
-
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ('ext_fctabout_http_proxy_rule_1', 1,
-'/describe/([^/\?\&]*)?/?([^/\?\&:]*)/(.*)', vector ('force', 'login', 'url'), 2,
-'/describe?url=%U&force=%U&login=%U', vector ('url', 'force', 'login'), null, null, 2);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ('ext_fctabout_http_proxy_rule_2', 1,
-    '/describe/html/(.*)', vector ('g'), 1,
-    '/fct/rdfdesc/description.vsp?g=%U', vector ('g'), null, null, 2);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ('ext_fctabout_http_proxy_rule_3', 1,
-    '/describe/\\?url=(.*)', vector ('g'), 1,
-    '/fct/rdfdesc/description.vsp?g=%s', vector ('g'), null, null, 2);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ('ext_fctabout_http_proxy_rule_4', 1,
-    '/describe/\\?uri=(.*)', vector ('g'), 1,
-    '/fct/rdfdesc/description.vsp?g=%s', vector ('g'), null, null, 2);
-
-DB.DBA.URLREWRITE_CREATE_REGEX_RULE ('ext_fctabout_http_proxy_rule_5', 1,
-    '/describe/\\?uri=([^\&]*)\&graph=([^\&]*)', vector ('g', 'graph'), 2,
-    '/fct/rdfdesc/description.vsp?g=%s&graph=%s', vector ('g', 'graph'), null, null, 2);
-
-DB.DBA.URLREWRITE_CREATE_RULELIST ('ext_fctabout_http_proxy_rule_list1', 1, 
-    vector ('ext_fctabout_http_proxy_rule_1', 
-      	    'ext_fctabout_http_proxy_rule_2', 
-	    'ext_fctabout_http_proxy_rule_3', 
-	    'ext_fctabout_http_proxy_rule_4',
-	    'ext_fctabout_http_proxy_rule_5'
-	    ));
-
-DB.DBA.VHOST_REMOVE (lpath=>'/describe');
-DB.DBA.VHOST_DEFINE (lpath=>'/describe', ppath=>'/SOAP/Http/EXT_HTTP_PROXY_1', soap_user=>'PROXY',
-    opts=>vector('url_rewrite', 'ext_fctabout_http_proxy_rule_list1'));
--- sql/facet_view.sql
+++ sql/facet_view.sql
@@ -552,21 +552,6 @@ fct_query_info (in tree any,
 }
 ;
 
-VHOST_REMOVE (lpath=>'/fct');
-VHOST_DEFINE (
-  lpath=>'/fct',
-    	ppath=>case when registry_get('_fct_path_') = 0 then '/fct/' else registry_get('_fct_path_') end, 
-	is_dav=>atoi (case when registry_get('_fct_dav_') = 0 then '0' else registry_get('_fct_dav_') end),
-  vsp_user=>'dba',
-  def_page=>'facet.vsp',
-  opts=>vector ('401_page', 'login.vsp', '403_page', 'login.vsp'));
-VHOST_REMOVE (lpath=>'/b3s');
-VHOST_DEFINE (lpath=>'/b3s',
-    	ppath=>case when registry_get('_fct_path_') = 0 then '/fct/' else registry_get('_fct_path_') end || 'www/', 
-	is_dav=>atoi (case when registry_get('_fct_dav_') = 0 then '0' else registry_get('_fct_dav_') end),
-    	vsp_user=>'dba', def_page=>'listall.vsp');
-
-
 create procedure
 fct_top (in tree any, in txt any)
 {
